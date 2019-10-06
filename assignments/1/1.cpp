85186be8bbfc54985ad1c6fe3dc5885e5156f3a9
garrett <martin.garrettl@gmail.com>
2019-10-06 12:17:40 -0700

    design additions, fix code organization

--- assignments/1/1.cpp
+++ assignments/1/1.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 int main () {
 // initializing variables
-  int salary, months, car_cost, car_sold, misconduct, taxyear,
-  annual, deduction, grossincome, netincome, avgsale;
+  int salary, months, car_cost, car_sold, misconduct = 0, taxyear,
+  annual, deduction, grossincome, avgsale;
   float random, profit, taxrate;
   char workstate;
 
@@ -41,7 +41,6 @@ int main () {
   avgsale = car_cost * (1 + ((rand()%5 + 5) / 100.0));
   profit = car_sold * (avgsale - car_cost);
 
-  deduction = 0;
   if (misconduct > 0) {
     deduction = 100 * pow(2, misconduct - 1);
   }
@@ -55,62 +54,79 @@ int main () {
     if (workstate == 'A') {
       cout << "and working in state A: " << endl;
       taxrate = grossincome * 0.06;
-    } else if (workstate == 'B'){
+    }
+    else if (workstate == 'B'){
       cout << "and working in state B: " << endl;
       if (grossincome < 2000) {
         taxrate = 0;
-      } else if (grossincome < 10000) {
+      }
+      else if (grossincome < 10000) {
         taxrate = 100;
-      } else {
+      }
+      else {
         taxrate = (grossincome * 0.1) + 100;
       }
-    } else if (workstate == 'C'){
+    }
+    else if (workstate == 'C'){
       cout << "and working in state C: " << endl;
       if (grossincome < 3500) {
         taxrate = grossincome * 0.05;
-      } else if (grossincome < 9000) {
+      }
+      else if (grossincome < 9000) {
         taxrate = (grossincome * 0.07) + 175;
-      } else if (grossincome < 125000) {
+      }
+      else if (grossincome < 125000) {
         taxrate = (grossincome * 0.09) + 560;
-      } else {
+      }
+      else {
         taxrate = (grossincome * 0.099) + 11000;
       }
     } else {
-      cout << "Invalid state provided" << endl;
+      cout << endl << "Invalid state provided" << endl;
       return 0;
     }
   //2018
-} else if (taxyear == 2018) {
+}
+else if (taxyear == 2018) {
     cout << "For tax year 2018 ";
     if (workstate == 'A') {
       cout << "and working in state A: " << endl;
       taxrate = (grossincome * 0.08);
-    } else if (workstate == 'B'){
+    }
+    else if (workstate == 'B'){
       cout << "and working in state B: " << endl;
       if (grossincome < 2500) {
         taxrate = 0;
-      } else if (grossincome < 10000) {
+      }
+      else if (grossincome < 10000) {
         taxrate = 115;
-      } else {
+      }
+      else {
         taxrate = (grossincome * 0.105) + 115;
       }
-    } else if (workstate == 'C'){
+    }
+    else if (workstate == 'C'){
       cout << "and working in state C: " << endl;
       if (grossincome < 3450) {
         taxrate = grossincome * 0.05;
-      } else if (grossincome < 8700) {
+      }
+      else if (grossincome < 8700) {
         taxrate = (grossincome * 0.07) + 172.5;
-      } else if (grossincome < 125000) {
+      }
+      else if (grossincome < 125000) {
         taxrate = (grossincome * 0.09) + 540;
-      } else {
+      }
+      else {
         taxrate = (grossincome * 0.099) + 11007;
       }
-    } else {
+    }
+    else {
       cout << "Invalid state provided" << endl;
       return 0;
     }
-  } else {
-    cout << "invalid tax year provided" << endl;
+  }
+  else {
+    cout << endl << "invalid tax year provided" << endl;
     return 0;
   }
 // end tax calculations
