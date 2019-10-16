41f962e2c4034c6bda5498e9391d9a1e19301657
Garrett <martin.garrettl@gmail.com>
2019-10-16 11:18:18 -0700

    add assignment 2

---
+++ assignments/2/2.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+**Program: assignment2.cpp
+**Author: Garrett Martin
+**Date: 10/16/19
+**Description:
+**Input:
+**Output:
+******************************************************************************/
+
+#include <iostream> // cin.clear(), cin.ignore(), cin.fail()
+#include <string> // getline(), .length(), size(), .at()
+#include <cmath> // pow(), abs()
+
+main() {
+
+
+
+  return 0;
+}
