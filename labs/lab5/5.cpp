bd91b33845046fcf683d75a8db23be6ebc6e682d
garrett <martin.garrettl@gmail.com>
2019-10-28 13:45:28 -0700

    update

--- labs/lab5/5.cpp
+++ labs/lab5/5.cpp
@@ -3,12 +3,25 @@
 
 using namespace std;
 
-void get_sentence(string &s) {
-
-}
+//declare get_sentence
+void get_sentence(string& s);
 
 int main() {
+  string sentence;
 
+  sentence = get_sentence(sentence); //call get_sentence
+  cout << sentence;
 
   return 0;
+}
+
+//define get_sentence
+
+void get_sentence(string& s) {
+  string newnew = s;
+
+  newnew = getline(cin, s);
+  s = newnew;
+
+  return;
 }
