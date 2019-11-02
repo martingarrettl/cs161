d36978ae18bdad56cf906a8fac5d23c1f32baf24
Garrett <martin.garrettl@gmail.com>
2019-11-02 13:29:31 -0700

    stuff

--- assignments/3/fractal.cpp
+++ assignments/3/fractal.cpp
@@ -17,20 +17,33 @@ Pre-Conditions: takes two int as parameters
 Post-Conditions: Print a fractal pattern to console
 ***************************************************************************/
 void pattern(int n, int col) {
-  if is_odd(n) == true; {
-    for (int j=0; j < n; j++) {
-      for (int i; i < n; i++)  cout << "*";
-      for (int i; i < col; i++) cout << " ";
-    }
-  } else {
-    cout << "ERROR: Incorrect n\nInput an odd integer: ";
-    gimmean_int(n);
-  }
+  int repeat=1;
+    for (int i = col; i < n; i++) cout << " ";
+    for (int i = n; )
+    cout << endl;
+    for (int i = 0; i , 2*n ; i++) pattern();
 }
+// Description:
+// The longest line of the pattern has n starts beginning in column col of the output.
+// For example, the above pattern is produce by the call pattern(7, 0).
 
 int main() {
-
-  pattern(7,0);
+  int n=7, col=0, repeat=0;
+/*
+  cout << "n (odd number): ";
+  gimmean_int(n);
+  while (repeat==0){
+    if (is_odd(n)==true) {
+      repeat=1;
+    } else {
+      "ERROR: Incorrect n\nInput an odd integer: ";
+      gimmean_int(n);
+    }
+  }
+  cout << "col: ";
+  gimmean_int(col);
+*/
+  pattern(n, col);
 
   return 0;
 }
