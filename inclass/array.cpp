9f5f0b016caaafed34115bce442e9a379be4a8d9
Garrett <martin.garrettl@gmail.com>
2019-11-18 11:02:26 -0800

    update in class stuff

--- inclass/array.cpp
+++ inclass/array.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 void init(int *arr, int size);
 void print_arr(int *arr, int size);
+void create_mem2(int &ref, int size);
+
 
 int main() {
   int arr[5]; // initialize array with length 5 and type of integer
   int *array; //
+  int *array2;
+  int *array3;
 
+
+  create_mem2(array2, 5);
+  create_mem3(array3, 20);
   array =  new int [5];
   delete [] array;
 
@@ -26,4 +33,14 @@ void print_arr(int *arr, int size) {
 int *create_mem(int size) {
   int *arr = new int[size];
   return arr;
+}
+
+//pass pointer to function as reference, ptr is ref to an int*
+void create_mem3(int *&heap, int size) {
+
+}
+
+//pass address of pointer to the int
+void create_mem2(int ** ptr, int size) { //ptr to a ptr
+  *ptr = new int [size];
 }
---
+++ inclass/cstrings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+  char cstring[6] = {"hello\0"}
+  string cppstring = cstring // valid, but c++ to c is not without conversion
+  return 0;
+}
